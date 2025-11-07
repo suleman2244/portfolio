68f263273d9c73676b763098d76dec0bf512507a
omerspac <omerspac@gmail.com>
2025-11-07 14:35:49 +0500

    Added baseUrl to tsconfig.json for module resolution and corrected import name from NavBar to Navbar in page.tsx for consistency.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import Portfolio from "@/components/Portfolio";
 import ScrollToTopButton from "@/components/ScrollToTop";
-import NavBar from '@/components/Navbar';
+import Navbar from '@/components/Navbar';
 
 export default function Home() {
   return (
     <>
-    <NavBar />
+    <Navbar />
     <Portfolio />
     <ScrollToTopButton />
     </>
