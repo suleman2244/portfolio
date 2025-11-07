2983ffe814116d557a31075a380f3fbeaaab62de
omerspac <omerspac@gmail.com>
2025-11-07 14:39:27 +0500

    Updated import paths in page.tsx for consistency by changing from absolute to relative paths.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import '@fortawesome/fontawesome-free/css/all.min.css'
-import Portfolio from "@/components/Portfolio";
-import ScrollToTopButton from "@/components/ScrollToTop";
-import Navbar from '@/components/Navbar';
+import Portfolio from "../components/Portfolio";
+import ScrollToTopButton from "../components/ScrollToTop";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
